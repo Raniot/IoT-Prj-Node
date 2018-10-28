22544befdc8304727a406de75435cba5b0c27d55
Anders Meidahl <Andersmeidahl@gmail.com>
2018-10-28 16:44:39 +0100

    Moved and fixed stuff for PiCameraTest

--- PiCameraTest.py
+++ PiCameraTest.py
@@ -5,13 +5,17 @@
 from pyimagesearch.tempimage import TempImage
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from imutils.object_detection import non_max_suppression
+import numpy as np
 import argparse
 import warnings
 import datetime
 import imutils
+import dlib
 import json
 import time
 import cv2
+
 
 # construct the argument parser and parse the arguments
 #ap = argparse.ArgumentParser()
@@ -48,44 +52,48 @@ skip_frames = 240
 #lastUploaded = datetime.datetime.now()
 #motionCounter = 0
 
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+trackers = []
 
 # capture frames from the camera
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
 	image = f.array
-    # load the image and resize it to (1) reduce detection time
-    # and (2) improve detection accuracy
+	# load the image and resize it to (1) reduce detection time
+	# and (2) improve detection accuracy
 	image = imutils.resize(image, width=min(400, image.shape[1]))
 	if totalFrames % skip_frames == 0:
 		print("Trying to detect")
 
-        # orig = image.copy()
+		# orig = image.copy()
 
-        # detect people in the image
-        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-            padding=(8, 8), scale=1.05)
+		# detect people in the image
+		(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+			padding=(8, 8), scale=1.05)
 
-        # draw the original bounding boxes
-        # for (x, y, w, h) in rects:
-        #     cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+		# draw the original bounding boxes
+		# for (x, y, w, h) in rects:
+		#	 cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-        # apply non-maxima suppression to the bounding boxes using a
-        # fairly large overlap threshold to try to maintain overlapping
-        # boxes that are still people
-        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+		# apply non-maxima suppression to the bounding boxes using a
+		# fairly large overlap threshold to try to maintain overlapping
+		# boxes that are still people
+		rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+		pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-        # draw the final bounding boxes
-        for (xA, yA, xB, yB) in pick:
-            cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
-            tracker = dlib.correlation_tracker()
-            rect = dlib.rectangle(xA, yA, xB, yB)
-            tracker.start_track(image, rect)
-            # add the tracker to our list of trackers so we can
-            # utilize it during skip frames
-            trackers.append(tracker)
-        
+		# draw the final bounding boxes
+		for (xA, yA, xB, yB) in pick:
+			cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+			tracker = dlib.correlation_tracker()
+			rect = dlib.rectangle(xA, yA, xB, yB)
+			tracker.start_track(image, rect)
+			# add the tracker to our list of trackers so we can
+			# utilize it during skip frames
+			trackers.append(tracker)
+		
 	else:
 		# loop over the trackers
 		for tracker in trackers:
@@ -104,11 +112,11 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 			endY = int(pos.bottom())
 
 			# add the bounding box coordinates to the rectangles list
-            # rects1.append((startX, startY, endX, endY))
+			# rects1.append((startX, startY, endX, endY))
 			cv2.rectangle(image,(startX,startY),(endX,endY),(255,0,0),2)
 
-    # show the output images
-    # cv2.imshow("Before NMS", orig)
+	# show the output images
+	# cv2.imshow("Before NMS", orig)
 	cv2.imshow('Frame',image)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
