89e7642a842e4fcbf1b9fe7e688d19368b234fda
Anders Meidahl <Andersmeidahl@gmail.com>
2018-11-07 21:59:45 +0100

    Updated with BackgroundScript

--- node/app.js
+++ node/app.js
@@ -3,22 +3,11 @@ const app = express()
 const port = 3000
 
 const spawn = require("child_process").spawn;
-const pythonProcess = spawn('python',["script.py"]);
+const pythonProcess = spawn('python',["BackgroundSubtraction.py"]);
 
 var people = 0
 
-// pythonProcess.stdout.on('data', (data) => {
-//   // Do something with the data returned from python script
-
-//   var integer = parseInt(data);
-//   people += integer
-//   // console.log(data.toString());
-//   // test = data.toString()
-// });
-
 pythonProcess.stdout.on('data', function(data) {
-  // Do something with the data returned from python script
-  // console.log(data.toString());
   var integer = parseInt(data);
   people += integer
 });
